3a209ec82b164f54a9d00396cee14f12c4a0e865
VIET-GITL <viet.info.43@gmail.com>
2024-04-23 07:21:28 +0700

    feat login

--- lofi/src/screens/home/home.js
+++ lofi/src/screens/home/home.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 import Feed from '../feed/feed'
+import Login from '../auth/login'
 import Player from '../player/player'
 import Library from '../library/library'
 import Trending from '../trending/trending'
@@ -9,19 +11,41 @@ import Favorites from '../favorites/favorites'
 import '../home/home.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sidebar from '../../components/sidebar'
+import { setClientToken } from '../../spotify'
+
 
 export default function Home() {
-  return (
+  const [token,setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token")
+    const hash = window.location.hash
+    window.location.hash='' 
+    if(!token && hash){
+      const _token = hash.split('&')[0].split('=')[1]
+      window.localStorage.setItem("token", _token)
+      setToken(_token)
+      setClientToken(_token)
+    }
+    else{
+      setToken(token)
+      setClientToken(token)
+    }
+    
+  }, [])
+  return !token ?(<Login/>) :
+   (
     <Router>
       <div className='main-body'>
       <Sidebar/>
+   
       <Routes>
         <Route path='/' element={<Library />} />
         <Route path='/feed' element={<Feed />} />
         <Route path='/player' element={<Player />} />
         <Route path='/trending' element={<Trending />} />
         <Route path='/favorites' element={<Favorites />} />
-      </Routes>
+      </Routes> 
       </div>
     </Router>
   )
